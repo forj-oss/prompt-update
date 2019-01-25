d92d6c72164d79679afb852347f566161ded3d4a
Christophe Larsonneur <clarsonneur@gmail.com>
2019-01-25 14:24:40 +0100

    Support for prompt handler update

--- bin/prompt_addons.sh
+++ bin/prompt_addons.sh
@@ -4,14 +4,30 @@
 
 INFO=""
 
+if [[ $# -gt 2 ]]
+then
+   COLOR=$3
+   NORMAL_COLOR=$4
+   if [[ "$COLOR" = "" ]] || [[ "$NORMAL_COLOR" = "" ]] 
+   then
+      COLOR=""
+      NORMAL_COLOR=""
+   fi
+fi
+
+if [[ "$PROMPT_HANDLER_ADDONS" != "" ]]
+then
+    $PROMPT_HANDLER_ADDONS
+fi
+
 for i in $(set | grep -e "^PROMPT_ADDONS\w*=" | sed 's/^\(\w*\)=.*$/\1/g')
 do
-  eval "VAL=\"\$$i\""
+  eval "VAL=\"$COLOR\$$i$NORMAL_COLOR\""
   if [ "$INFO" = "" ]
   then
-     INFO="$(echo $VAL | tail -n 1)"
+     INFO="$(echo "$VAL" | tail -n 1)"
   else
-     INFO="${INFO}-$(echo $VAL | tail -n 1)"
+     INFO="${INFO} | $(echo "$VAL" | tail -n 1)"
   fi
 done
 if [ "$INFO" != "" ]
